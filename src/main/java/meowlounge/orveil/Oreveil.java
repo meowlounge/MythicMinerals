23858dd0f0cef3160d7e48a08c85acc48aabcfca
dwhincandi <andreifischer2006@gmail.com>
2025-07-08 14:26:18 +0300

    Test

--- src/main/java/meowlounge/orveil/Oreveil.java
+++ src/main/java/meowlounge/orveil/Oreveil.java
@@ -9,7 +9,7 @@ public class Oreveil implements ModInitializer {
 	public static final String MOD_ID = "oreveil";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+//dsadas
 	@Override
 	public void onInitialize() {
 	}
