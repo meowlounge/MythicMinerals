90c9678c2f0b2f20875912a9903ef760b3f34d17
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-08 22:16:27 +0200

    moved rile

--- src/main/java/meowlounge/oreveil/ModItems.java
+++ src/main/java/meowlounge/oreveil/ModItems.java
@@ -1,9 +1,6 @@
-package meowlounge.oreveil.item;
+package meowlounge.oreveil;
 
-import meowlounge.oreveil.Oreveil;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
@@ -14,7 +11,7 @@ import java.util.function.Function;
 
 public class ModItems {
     public static final Item RAW_FROSTIUM = registerItem("raw_frostium", Item::new);
-    public static final Item FROSTIUM_INGOT = registerItem("frostium_ingot", Item::new);
+    public static final Item FROSTIUM = registerItem("frostium", Item::new);
 
     private static Item registerItem(String name, Function<Item.Settings, Item> function) {
         return Registry.register(Registries.ITEM, Identifier.of(Oreveil.MOD_ID, name),
@@ -22,11 +19,6 @@ public class ModItems {
     }
 
     public static void registerModItems() {
-        Oreveil.LOGGER.info("Registering Mod Items for " + Oreveil.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(FROSTIUM_INGOT);
-            entries.add(RAW_FROSTIUM);
-        });
+        Oreveil.LOGGER.info("⛏️ [OREVEIL]: Registering Items");
     }
 }
