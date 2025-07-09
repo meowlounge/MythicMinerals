66604fa9d05a596ff27755e60eb6f223358058d6
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-09 20:18:16 +0200

    fixed worldgen crash

--- src/main/java/dev/meowlounge/mythicminerals/MythicMineralsDataGenerator.java
+++ src/main/java/dev/meowlounge/mythicminerals/MythicMineralsDataGenerator.java
@@ -5,8 +5,12 @@ import dev.meowlounge.mythicminerals.datagen.RegistryData;
 import dev.meowlounge.mythicminerals.datagen.lang.LangDE;
 import dev.meowlounge.mythicminerals.datagen.lang.LangEN;
 import dev.meowlounge.mythicminerals.datagen.loot_table.FrostiumLoot;
+import dev.meowlounge.mythicminerals.world.ConfiguredFeatures;
+import dev.meowlounge.mythicminerals.world.PlacedFeatures;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class MythicMineralsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -18,5 +22,11 @@ public class MythicMineralsDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(LangEN::new);
 		pack.addProvider(FrostiumLoot::new);
 		pack.addProvider(RegistryData::new);
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ConfiguredFeatures::bootstrap);
+		registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, PlacedFeatures::bootstrap);
 	}
 }
