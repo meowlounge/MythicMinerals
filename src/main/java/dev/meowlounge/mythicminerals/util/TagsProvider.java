01de57e910fffc51a717db1028e659d63bf0ccb0
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-10 16:44:01 +0200

    renamed file

--- src/main/java/dev/meowlounge/mythicminerals/util/TagsProvider.java
+++ src/main/java/dev/meowlounge/mythicminerals/util/TagsProvider.java
@@ -8,10 +8,10 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 
-public class Tags {
+public class TagsProvider {
     public static class Blocks {
-//        public static final TagKey<Block> NEEDS_FROSTIUM_TOOL = createTag("needs_frostium_tool");
-//        public static final TagKey<Block> INCORRECT_FOR_FROSTIUM_TOOL = createTag("incorrect_for_frostium_tool");
+        public static final TagKey<Block> NEEDS_FROSTIUM_TOOL = createTag("needs_frostium_tool");
+        public static final TagKey<Block> INCORRECT_FOR_FROSTIUM_TOOL = createTag("incorrect_for_frostium_tool");
 
         private static TagKey<Block> createTag(String name) {
             return TagKey.of(RegistryKeys.BLOCK, Identifier.of(MythicMinerals.MOD_ID, name));
