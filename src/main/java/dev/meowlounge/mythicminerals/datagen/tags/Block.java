5ec94c14442caebb3cb493a93a60ffd77e319ec1
dwhincandi <andreifischer2006@gmail.com>
2025-07-11 16:17:30 +0300

    feat(datagen): updated

--- src/main/java/dev/meowlounge/mythicminerals/datagen/tags/Block.java
+++ src/main/java/dev/meowlounge/mythicminerals/datagen/tags/Block.java
@@ -1,7 +1,9 @@
 package dev.meowlounge.mythicminerals.datagen.tags;
 
+import dev.meowlounge.mythicminerals.TagsProvider;
 import dev.meowlounge.mythicminerals.block.FrostiumBlocks;
-import dev.meowlounge.mythicminerals.block.PhantomiumBlocks;
+import dev.meowlounge.mythicminerals.block.StellariumBlocks;
+import dev.meowlounge.mythicminerals.block.VoidstoneBlocks;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.registry.RegistryWrapper;
@@ -21,9 +23,11 @@ public class Block extends FabricTagProvider.BlockTagProvider {
                 .add(FrostiumBlocks.RAW_FROSTIUM_BLOCK)
                 .add(FrostiumBlocks.FROSTIUM_BLOCK)
                 .add(FrostiumBlocks.DEEPSLATE_FROSTIUM_ORE)
-                .add(PhantomiumBlocks.PHANTOMIUM_ORE)
-                .add(PhantomiumBlocks.RAW_PHANTOMIUM_BLOCK)
-                .add(PhantomiumBlocks.PHANTOMIUM_BLOCK);
+                .add(VoidstoneBlocks.VOIDSTONE_ORE)
+                .add(VoidstoneBlocks.RAW_VOIDSTONE_BLOCK)
+                .add(VoidstoneBlocks.VOIDSTONE_BLOCK)
+                .add(StellariumBlocks.STELLARIUM_ORE)
+                .add(StellariumBlocks.STELLARIUM_BLOCK);
 
         valueLookupBuilder(BlockTags.NEEDS_IRON_TOOL)
                 .add(FrostiumBlocks.FROSTIUM_ORE)
@@ -32,9 +36,31 @@ public class Block extends FabricTagProvider.BlockTagProvider {
                 .add(FrostiumBlocks.DEEPSLATE_FROSTIUM_ORE);
 
         valueLookupBuilder(BlockTags.NEEDS_DIAMOND_TOOL)
-                .add(PhantomiumBlocks.PHANTOMIUM_ORE)
-                .add(PhantomiumBlocks.RAW_PHANTOMIUM_BLOCK)
-                .add(PhantomiumBlocks.PHANTOMIUM_BLOCK);
+                .add(VoidstoneBlocks.VOIDSTONE_ORE)
+                .add(VoidstoneBlocks.RAW_VOIDSTONE_BLOCK)
+                .add(VoidstoneBlocks.VOIDSTONE_BLOCK);
+
+        valueLookupBuilder(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL)
+                .add(StellariumBlocks.STELLARIUM_ORE)
+                .add(StellariumBlocks.STELLARIUM_BLOCK);
+
+        valueLookupBuilder(BlockTags.INCORRECT_FOR_DIAMOND_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
+
+        valueLookupBuilder(BlockTags.INCORRECT_FOR_IRON_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
+
+        valueLookupBuilder(BlockTags.INCORRECT_FOR_STONE_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
+
+        valueLookupBuilder(BlockTags.INCORRECT_FOR_GOLD_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
+
+        valueLookupBuilder(BlockTags.INCORRECT_FOR_WOODEN_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
+
+        valueLookupBuilder(TagsProvider.Blocks.INCORRECT_FOR_FROSTIUM_TOOL)
+                .addTag(TagsProvider.Blocks.NEEDS_NETHERITE_TOOL);
     }
 
     @Override
