fe9263c66049de1f8c661d5d1517ca2ed462f668
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-12 00:24:42 +0200

    feat(RegisterProvider): moved RegisterBlock/RegisterItem to one file.

--- src/main/java/dev/meowlounge/mythicminerals/providers/RegisterProvider.java
+++ src/main/java/dev/meowlounge/mythicminerals/providers/RegisterProvider.java
@@ -1,4 +1,4 @@
-package dev.meowlounge.mythicminerals.block;
+package dev.meowlounge.mythicminerals.providers;
 
 import dev.meowlounge.mythicminerals.MythicMinerals;
 import net.minecraft.block.AbstractBlock;
@@ -13,7 +13,21 @@ import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
 
-public class RegisterBlock {
+public class RegisterProvider {
+	public static Item registerItem(String name, Function<Item.Settings, Item> function) {
+		return Registry.register(Registries.ITEM,
+				Identifier.of(MythicMinerals.MOD_ID, name),
+				function.apply(new Item.Settings().registryKey(
+						RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MythicMinerals.MOD_ID, name)))
+				)
+		);
+	}
+	public static RegistryKey<Item> createItemKey(String name) {
+		return RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(MythicMinerals.MOD_ID, name));
+	}
+
+	// ---------------------- //
+
 	public static Block registerBlock(String name, Function<AbstractBlock.Settings, Block> function) {
 		Block toRegister = function.apply(AbstractBlock.Settings.create().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(MythicMinerals.MOD_ID, name))));
 		registerBlockItem(name, toRegister);
