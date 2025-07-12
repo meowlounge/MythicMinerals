9aeb7780d50923aec9fe49bfc04bebcebac22bd4
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-12 23:24:26 +0200

    chore(Items): added Raritys

--- src/main/java/dev/meowlounge/mythicminerals/item/StellariumItems.java
+++ src/main/java/dev/meowlounge/mythicminerals/item/StellariumItems.java
@@ -1,5 +1,7 @@
 package dev.meowlounge.mythicminerals.item;
 
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.text.Style;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
@@ -20,40 +22,51 @@ public class StellariumItems {
             BlockTags.INCORRECT_FOR_NETHERITE_TOOL, 3000, 25.0F, 3.0F, 15, TagsProvider.Items.STELLARIUM_REPAIR
     );
 
+    public static final Item STELLARIUM_SCRAP = registerItem("stellarium_scrap",
+            settings -> new Item(settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
 
-    public static final Item STELLARIUM_SCRAP = registerItem("stellarium_scrap", Item::new);
-    public static final Item STELLARIUM = registerItem("stellarium", Item::new);
+    public static final Item STELLARIUM = registerItem("stellarium",
+            settings -> new Item(settings.component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_SWORD = registerItem("stellarium_sword",
-            settings -> new Item(settings.sword(StellariumItems.STELLARIUM_TOOL_MATERIAL, 7, -2.2F)));
+            settings -> new Item(settings.sword(STELLARIUM_TOOL_MATERIAL, 7, -2.2F)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_SHOVEL = registerItem("stellarium_shovel",
-            settings -> new ShovelItem(StellariumItems.STELLARIUM_TOOL_MATERIAL, -0.5F, -3, settings));
+            settings -> new ShovelItem(STELLARIUM_TOOL_MATERIAL, -0.5F, -3,
+                    settings.component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_PICKAXE = registerItem("stellarium_pickaxe",
-            settings -> new Item(settings.pickaxe(StellariumItems.STELLARIUM_TOOL_MATERIAL, -0.5F, -3)));
+            settings -> new Item(settings.pickaxe(STELLARIUM_TOOL_MATERIAL, -0.5F, -3)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_AXE = registerItem("stellarium_axe",
-            settings -> new AxeItem(StellariumItems.STELLARIUM_TOOL_MATERIAL, 8, -2.8F, settings));
+            settings -> new AxeItem(STELLARIUM_TOOL_MATERIAL, 8, -2.8F,
+                    settings.component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_HOE = registerItem("stellarium_hoe",
-            settings -> new HoeItem(StellariumItems.STELLARIUM_TOOL_MATERIAL, -0.5F, -3, settings));
+            settings -> new HoeItem(STELLARIUM_TOOL_MATERIAL, -0.5F, -3,
+                    settings.component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
-    //* ---------------------- //
+    // ---------------------- //
 
     public static final Item STELLARIUM_HELMET = registerItem("stellarium_helmet",
-            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.HELMET)));
+            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.HELMET)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_CHESTPLATE = registerItem("stellarium_chestplate",
-            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.CHESTPLATE)));
+            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.CHESTPLATE)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_LEGGINGS = registerItem("stellarium_leggings",
-            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.LEGGINGS)));
+            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.LEGGINGS)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
     public static final Item STELLARIUM_BOOTS = registerItem("stellarium_boots",
-            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.BOOTS)));
+            settings -> new Item(settings.armor(Armor.STELLARIUM_ARMOR_MATERIAL, EquipmentType.BOOTS)
+                    .component(DataComponentTypes.RARITY, Rarity.EPIC)));
 
-    //* ---------------------- //
+    // ---------------------- //
 
     public static final Item STELLARIUM_UPGRADE_SMITHING_TEMPLATE = registerItem("stellarium_upgrade_smithing_template",
             settings -> new SmithingTemplateItem(
@@ -76,8 +89,6 @@ public class StellariumItems {
                             .registryKey(createItemKey("stellarium_upgrade_smithing_template"))
                             .rarity(Rarity.EPIC)
             ));
-
-    //* ---------------------- //
 
     public static void registerStellariumItems() {
         MythicMinerals.LOGGER.info("⛏️ [MythicMinerals]: Registering Stellarium Items");
--- src/main/java/dev/meowlounge/mythicminerals/item/VoidstoneItems.java
+++ src/main/java/dev/meowlounge/mythicminerals/item/VoidstoneItems.java
@@ -2,9 +2,11 @@ package dev.meowlounge.mythicminerals.item;
 
 import dev.meowlounge.mythicminerals.MythicMinerals;
 import dev.meowlounge.mythicminerals.providers.TagsProvider;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.*;
 import net.minecraft.item.equipment.EquipmentType;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.Rarity;
 
 import static dev.meowlounge.mythicminerals.providers.RegisterProvider.registerItem;
 
@@ -13,37 +15,49 @@ public class VoidstoneItems {
             BlockTags.INCORRECT_FOR_DIAMOND_TOOL, 1700, 15.0F, 3.0F, 15, TagsProvider.Items.VOIDSTONE_REPAIR
     );
 
-    public static final Item RAW_VOIDSTONE = registerItem("raw_voidstone", Item::new);
-    public static final Item VOIDSTONE = registerItem("voidstone", Item::new);
+    public static final Item RAW_VOIDSTONE = registerItem("raw_voidstone",
+            settings -> new Item(settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
+
+    public static final Item VOIDSTONE = registerItem("voidstone",
+            settings -> new Item(settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_SWORD = registerItem("voidstone_sword",
-            settings -> new Item(settings.sword(VoidstoneItems.VOIDSTONE_TOOL_MATERIAL, 5, -2.2F)));
+            settings -> new Item(settings.sword(VOIDSTONE_TOOL_MATERIAL, 5, -2.2F)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_SHOVEL = registerItem("voidstone_shovel",
-            settings -> new ShovelItem(VoidstoneItems.VOIDSTONE_TOOL_MATERIAL, -0.5F, -3, settings));
+            settings -> new ShovelItem(VOIDSTONE_TOOL_MATERIAL, -0.5F, -3,
+                    settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_PICKAXE = registerItem("voidstone_pickaxe",
-            settings -> new Item(settings.pickaxe(VoidstoneItems.VOIDSTONE_TOOL_MATERIAL, -0.5F, -3)));
+            settings -> new Item(settings.pickaxe(VOIDSTONE_TOOL_MATERIAL, -0.5F, -3)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_AXE = registerItem("voidstone_axe",
-            settings -> new AxeItem(VoidstoneItems.VOIDSTONE_TOOL_MATERIAL, 7, -2.8F, settings));
+            settings -> new AxeItem(VOIDSTONE_TOOL_MATERIAL, 7, -2.8F,
+                    settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_HOE = registerItem("voidstone_hoe",
-            settings -> new HoeItem(VoidstoneItems.VOIDSTONE_TOOL_MATERIAL, -0.5F, -3, settings));
+            settings -> new HoeItem(VOIDSTONE_TOOL_MATERIAL, -0.5F, -3,
+                    settings.component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     //* ---------------------- //
 
     public static final Item VOIDSTONE_HELMET = registerItem("voidstone_helmet",
-            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.HELMET)));
+            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.HELMET)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_CHESTPLATE = registerItem("voidstone_chestplate",
-            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.CHESTPLATE)));
+            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.CHESTPLATE)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_LEGGINGS = registerItem("voidstone_leggings",
-            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.LEGGINGS)));
+            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.LEGGINGS)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     public static final Item VOIDSTONE_BOOTS = registerItem("voidstone_boots",
-            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.BOOTS)));
+            settings -> new Item(settings.armor(Armor.VOIDSTONE_ARMOR_MATERIAL, EquipmentType.BOOTS)
+                    .component(DataComponentTypes.RARITY, Rarity.RARE)));
 
     //* ---------------------- //
 
