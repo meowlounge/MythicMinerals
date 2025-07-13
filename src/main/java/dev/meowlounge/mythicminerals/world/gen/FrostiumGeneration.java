30056e33fa031f767494ac17809b423c8b827ee0
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-13 13:54:51 +0200

    changed some biome stuff

--- src/main/java/dev/meowlounge/mythicminerals/world/gen/FrostiumGeneration.java
+++ src/main/java/dev/meowlounge/mythicminerals/world/gen/FrostiumGeneration.java
@@ -3,15 +3,25 @@ package dev.meowlounge.mythicminerals.world.gen;
 import dev.meowlounge.mythicminerals.world.PlacedFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
 public class FrostiumGeneration {
 	public static void generateOres() {
 		BiomeModifications.addFeature(
-				BiomeSelectors.foundInOverworld().and(context -> {
-					var biome = context.getBiome();
-					return biome.getTemperature() <= 0.2F; // ez way to get the biomes...
-				}),
+				BiomeSelectors.includeByKey(
+						BiomeKeys.SNOWY_PLAINS,
+						BiomeKeys.SNOWY_TAIGA,
+						BiomeKeys.ICE_SPIKES,
+						BiomeKeys.FROZEN_OCEAN,
+						BiomeKeys.DEEP_FROZEN_OCEAN,
+						BiomeKeys.FROZEN_RIVER,
+						BiomeKeys.GROVE,
+						BiomeKeys.SNOWY_SLOPES,
+						BiomeKeys.JAGGED_PEAKS,
+						BiomeKeys.FROZEN_PEAKS,
+						BiomeKeys.STONY_PEAKS
+				),
 				GenerationStep.Feature.UNDERGROUND_ORES,
 				PlacedFeatures.FROSTIUM_PLACED_KEY
 		);
