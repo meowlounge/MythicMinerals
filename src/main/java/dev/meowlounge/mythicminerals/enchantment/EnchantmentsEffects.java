3e846a09677981356f2e0219d291e5a58e1cd05d
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-17 23:06:05 +0200

    fix: something was broken. had to fix it

--- src/main/java/dev/meowlounge/mythicminerals/enchantment/EnchantmentsEffects.java
+++ src/main/java/dev/meowlounge/mythicminerals/enchantment/EnchantmentsEffects.java
@@ -2,12 +2,16 @@ package dev.meowlounge.mythicminerals.enchantment;
 
 import com.mojang.serialization.MapCodec;
 import dev.meowlounge.mythicminerals.MythicMinerals;
+import dev.meowlounge.mythicminerals.enchantment.custom.LavaWalkerEffect;
 import net.minecraft.enchantment.effect.EnchantmentEntityEffect;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class EnchantmentsEffects {
+	public static final MapCodec<? extends EnchantmentEntityEffect> LAVA_WALKER =
+			registerEntityEffect("lava_walker", LavaWalkerEffect.CODEC);
+
 	@SuppressWarnings({"SameParameterValue", "unused"})
 	private static MapCodec<? extends EnchantmentEntityEffect> registerEntityEffect(String name,
 																					MapCodec<? extends EnchantmentEntityEffect> codec) {
