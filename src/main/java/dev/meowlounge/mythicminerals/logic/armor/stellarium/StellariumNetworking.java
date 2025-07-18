64560f8fb2e15b1a659f90094259ed452ba42595
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-07-18 19:36:36 +0200

    refactor: renamed file

--- src/main/java/dev/meowlounge/mythicminerals/logic/armor/stellarium/StellariumNetworking.java
+++ src/main/java/dev/meowlounge/mythicminerals/logic/armor/stellarium/StellariumNetworking.java
@@ -3,8 +3,8 @@ package dev.meowlounge.mythicminerals.logic.armor.stellarium;
 import dev.meowlounge.mythicminerals.network.payload.StellariumBoostPayload;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 
-public class ModMessages {
-	public static void registerC2SPackets() {
+public class StellariumNetworking {
+	public static void registerStellariumPayload() {
 		ServerPlayNetworking.registerGlobalReceiver(StellariumBoostPayload.ID, (payload, context) -> StellariumLogic.tryBoost(context.player()));
 	}
 }
